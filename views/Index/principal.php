d37dba12c9276756d9146b98106f823349f1aeb4
Enrique Aguilar <enriqueao96@gmail.com>
2017-04-15 23:20:33 -0500

    Paquetado completo de la pagina principal, faltan agregar detalles, y corregir slider.

---
+++ views/Index/principal.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>SpaceProjects</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="<?=CSS?>estilosSpace.css">
+</head>
+<body class="principal">
+	<div class="top-bar"></div>
+	<div class="slider">
+		<ul>
+			<li><img src="<?=IMG?>developers.jpg"></li>
+			<li><img src="<?=IMG?>desk.jpg"></li>
+			<li><img src="<?=IMG?>webstr.jpg"></li>
+		</ul>
+	</div>
+	<div class="proyectos-principal"></div>
+	<div class="registro-principal"></div>
+
+	<footer></footer>
+</body>
+</html>
