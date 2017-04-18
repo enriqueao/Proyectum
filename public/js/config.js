c6e60465ff7641534fd0f2301ec76bc47e7619f8
Enrique Aguilar <enriqueao96@gmail.com>
2017-04-17 21:21:24 -0500

    Logo Corregido

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,7 @@
 var config = {
-			url: "http://10.195.103.103/SpaceProjects/",
-			img: "http://10.195.103.103/SpaceProjects/public/images/"
+			url: "http://localhost/SpaceProjects/",
+			img: "http://localhost/SpaceProjects/public/images/"
 		}
 		var imported = document.createElement("script");
-		imported.src = "http://10.195.103.103/SpaceProjects/public/js/complementos.js";
+		imported.src = "http://localhost/SpaceProjects/public/js/complementos.js";
 		document.head.appendChild(imported);
