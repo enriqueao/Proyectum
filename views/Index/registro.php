bc373554e1225379ff1de4229c7bca9f56f020d7
Enrique Aguilar <enriqueao96@gmail.com>
2017-06-14 11:24:37 -0500

    Cambio de estilos Registro

--- views/Index/registro.php
+++ views/Index/registro.php
@@ -5,27 +5,35 @@
     <meta http-equiv="pragma" content="no-cache">
     <title>Únete - Proyectum</title>
     <link rel="stylesheet" type="text/css" href="<?=CSS;?>estilosSpace.css">
+    <link rel="stylesheet" type="text/css" href="<?=CSS;?>subirProyecto.css">
 
   	<script type="text/javascript" src="<?=JS;?>config.js"></script>
     <script defer type="text/javascript" src="<?=JS;?>registro.js"></script>
   </head>
   <body>
     <?=$this->render('Default','userorlogin',true);?>
-    <div class="registro">
-      <div class="formulario-regitro">
-        <p>Nombre Completo</p>
-        <input class="registro-inputs" type="text" id="nombreCompleto" autocomplete="off">
-        <p>Nombre de Usuario</p>
-        <input class="registro-inputs" type="text" id="user" autocomplete="off">
-        <p id="usernamecomp">Introduce tu nombre de usuario mayor a 5 caracteres</p>
-        <p>Correo</p>
-        <input class="registro-inputs" type="text" id="correo" value="" autocomplete="off">
-        <p>Contraseña</p>
-        <input class="registro-inputs" type="password" id="pass" value="">
-        <p>Confirma Tu Contraseña</p>
-        <input class="registro-inputs" type="password" value="">
-        <input class="registro-submit" type="submit" id="botonSu" value="Registrarme" onclick="registrar()">
-      </div>
+    <div id="fakeBody">
+    <div id="contenedorPrincipal">
+    	<h1>Únete a Proyectum</h1>
+    	<div id="fondo">
+    		<form name="subirProyecto" onsubmit="return false">
+          <p>Nombre Completo</p>
+          <input class="registro-inputs" type="text" id="nombreCompleto" autocomplete="off">
+          <p>Nombre de Usuario</p>
+          <input class="registro-inputs" type="text" id="user" autocomplete="off">
+          <p id="usernamecomp">Introduce tu nombre de usuario mayor a 5 caracteres</p>
+          <p>Correo</p>
+          <input class="registro-inputs" type="text" id="correo" value="" autocomplete="off">
+          <p>Contraseña</p>
+          <input class="registro-inputs" type="password" id="pass" value="">
+          <p>Confirma Tu Contraseña</p>
+          <input class="registro-inputs" type="password" value="">
+    		<div id="contFinal">
+    			<button id="botonSu" onclick="registrar()">Registrarme</button>
+    		</div>
+    		</form>
+    	</div>
+    </div>
     </div>
     <?=$this->render('Index','footer',true);?>
   </body>
